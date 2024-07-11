f4d7124394100d564e9de1dd6b5c9121f9adb230
Christopher Pulte <cp526@cam.ac.uk>
2024-07-11 02:26:12 +0100

    progress on executable driver

--- driver.c
+++ driver.c
@@ -1,109 +1,50 @@
 #include "page_alloc.c"
 
-void *aligned_malloc(size_t align, size_t size);
-
-
-
-void cn_assume_ownership(void *generic_c_ptr, size_t size, char *fun);
-
-void *cn_aligned_malloc(size_t align, size_t size) 
-{
-  void *p = aligned_malloc(align, size);
-  char str[] = "cn_aligned_malloc";
-  cn_assume_ownership((void*) p, size, str);
-  return p;
-}
+void *cn_aligned_alloc(size_t align, size_t size);
 
 
 s64 hyp_physvirt_offset;
 
 
-void *malloc_hyp_page(unsigned int nr) 
-/* trusted;
-   requires nr > 0u32;
-   ensures mod ((u64) return, sizeof<struct hyp_page>) == 0u64;
-           take V = each (u64 i; 0u64 <= i && i < (u64) nr)
-                    {Block<struct hyp_page>(array_shift<struct hyp_page>(return, i)) };
-*/
+int main()
+/*@ accesses hyp_physvirt_offset; __hyp_vmemmap; cn_virt_ptr; @*/
 {
-  return aligned_malloc(sizeof(struct hyp_page), 
-                        sizeof(struct hyp_page) * nr);
-}
 
-void *malloc_init_hyp_page(unsigned int nr)
-/* requires nr > 0u32;
-   ensures mod ((u64) return, sizeof<struct hyp_page>) == 0u64;
-           take V = each (u64 i; 0u64 <= i && i < (u64) nr)
-                    {Owned<struct hyp_page>(array_shift<struct hyp_page>(return, i)) };
-*/
-{
-  struct hyp_page* p = (struct hyp_page *) malloc_hyp_page(nr);
-
-  for (unsigned int i = 0; i < nr; i++) 
-  /* 
-  inv {nr} unchanged;
-      0u32 <= i; i <= nr;
-      mod ((u64) p, sizeof<struct hyp_page>) == 0u64;            
-      take V1 = each (u64 j; 0u64 <= j && j < (u64) i)
-                     {Owned<struct hyp_page>(array_shift<struct hyp_page>(p, j)) }; 
-      take V2 = each (u64 j; (u64) i <= j && j < (u64) nr)
-                     {Block<struct hyp_page>(array_shift<struct hyp_page>(p, j)) }; 
-  */
-  {
-    /* extract Block<struct hyp_page>, (u64) i; */
-    /* extract Owned<struct hyp_page>, (u64) i; */
-    p[i].refcount = 0;
-    p[i].order = 0;
-    p[i].flags = 0;
-  }
-
-  return p;
-
-}
+  s64 offset = PAGE_SIZE;
+  unsigned int nr_pages = 20;
+  unsigned int reserved_pages = 0;
+  u8 max_order = 10;
 
 
-void *malloc_pages(unsigned int nr_pages)
-/* trusted;
-    accesses hyp_physvirt_offset; cn_virt_ptr;
-    requires nr_pages > 0u32;
-             let ptr_phys_0 = cn__hyp_va(cn_virt_ptr, hyp_physvirt_offset, 0u64);
-    ensures let start = cn__hyp_pa(hyp_physvirt_offset, return);
-            mod (start, page_size ()) == 0u64;
-            let start_i = start / page_size ();
-            take P = each (u64 i; start_i <= i && i < start_i + (u64) nr_pages)
-                          { Page(array_shift<PAGE_SIZE_t>(ptr_phys_0, i), true, 0u8) }; 
-*/
-{
-  return aligned_malloc(PAGE_SIZE, PAGE_SIZE * nr_pages);
-}
-
-
-void setup(s64 offset, 
-           unsigned int nr_pages, 
-           unsigned int reserved_pages,
-           u8 max_order)
-/* accesses __hyp_vmemmap; hyp_physvirt_offset; cn_virt_ptr; */
-{
   hyp_physvirt_offset = offset;
-  __hyp_vmemmap = malloc_init_hyp_page(nr_pages);
-  void *start_virt = malloc_pages(nr_pages);
+  __hyp_vmemmap = cn_aligned_alloc(sizeof(struct hyp_page), sizeof(struct hyp_page) * nr_pages);
 
-  /* void* ptr_phys_0 = (void*) __hyp_va(0); */
+  /* unsigned int i = 0; */
+  /* while (i < nr_pages) { */
+  /*   __hyp_vmemmap[i].refcount = 0; */
+  /*   __hyp_vmemmap[i].order = 0; */
+  /*   __hyp_vmemmap[i].flags = 0; */
+  /*   i++; */
+  /* } */
+
+
+  void *start_virt = cn_aligned_alloc(PAGE_SIZE, PAGE_SIZE * nr_pages);
 
   phys_addr_t range_start = (phys_addr_t) __hyp_pa(start_virt);
 
-  struct hyp_pool poolv = { 0 };
-  poolv.range_start = range_start;
-  poolv.range_end = range_start + nr_pages * PAGE_SIZE;
-  poolv.max_order = max_order;
-
-  /* printf("Running hyp_pool_init"); */
-  hyp_pool_init(&poolv, hyp_phys_to_pfn(range_start), nr_pages, reserved_pages);
-  /* printf("Done."); */
-}
+  struct hyp_pool *pool = cn_aligned_alloc(sizeof(struct hyp_pool), sizeof(struct hyp_pool));
+  *pool = (struct hyp_pool){ 0 };
+  pool->range_start = range_start;
+  pool->range_end = range_start + nr_pages * PAGE_SIZE;
+  pool->max_order = max_order;
 
 
-int main () {
-  setup(PAGE_SIZE, 20, 0, 10);
+
+  hyp_pool_init(pool, hyp_phys_to_pfn(range_start), nr_pages, reserved_pages);
+
+
   return 0;
 }
+
+
+
--- run2.sh
+++
@@ -1,5 +0,0 @@
-rm -rf build/
-mkdir build
-clang-18 -E -P -CC driver.c > driver.pp.c
-cn driver.pp.c --output_decorated=driver.pp.exec.c --output_decorated_dir=build/ --with_ownership_checking --copy_source_dir
-clang-18 -I$OPAM_SWITCH_PREFIX/lib/cn/runtime/include  $OPAM_SWITCH_PREFIX/lib/cn/runtime/libcn.a build/driver.pp.exec.c build/cn.c
