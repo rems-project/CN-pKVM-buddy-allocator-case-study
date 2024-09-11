8037f044ca00099714da1637bc8c78157ec5d0ff
Rini Banerjee <rb2018@cam.ac.uk>
2024-09-11 11:10:57 +0100

    Update run.sh with subcommand

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 rm -rf build/
 mkdir build
 clang-18 -E -P -CC driver.c > driver.pp.c
-cn verify ./driver.pp.c --output-decorated=driver.pp.exec.c --output-decorated-dir=build/ --with-ownership-checking
+cn instrument ./driver.pp.c --output-decorated=driver.pp.exec.c --output-decorated-dir=build/ --with-ownership-checking
 clang-18 -g -O0 -std=gnu11 -I$OPAM_SWITCH_PREFIX/lib/cn/runtime/include  $OPAM_SWITCH_PREFIX/lib/cn/runtime/libcn.a build/driver.pp.exec.c build/cn.c -o build/driver.exec.o
